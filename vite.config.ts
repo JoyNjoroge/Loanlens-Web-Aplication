e400aa6b63842e6ce828caed1a109990adfdfdb6
JoyNjoroge <njorogejoy78@gmail.com>
2026-01-15 18:55:31 +0200

    TailwindCSS fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "./",   // THIS fixes relative paths for nested deploys
+  base: "./",
   plugins: [
     react(),
     tsconfigPaths(),
-    tailwindcss(),
   ],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name]-[hash].js`,
-        assetFileNames: `[name]-[hash][extname]`,
-      },
-    },
+    outDir: "dist",
   },
 })
